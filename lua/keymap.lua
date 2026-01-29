f07114a24d551673684b50ef97231370b0e47b59
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2026-01-29 15:02:22 +0000

    Feat: go tests and coverage support

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -11,9 +11,15 @@ vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
 
 -- Render Markdown toggle
 vim.keymap.set("n", "<leader>mr", function()
-	require('render-markdown').toggle()
+    require('render-markdown').toggle()
 end, { desc = "Toggle Rendered Markdown" })
 
 -- Go Tagger
 vim.keymap.set("v", "<leader>at", ":AddGoTags<CR>", { desc = "Add Go struct tags", silent = true })
 vim.keymap.set("v", "<leader>rt", ":RemoveGoTags<CR>", { desc = "Remove Go struct tags", silent = true })
+
+-- Go Coverage
+vim.keymap.set('n', '<leader>tc', '<Cmd>Coverage<CR>', { desc = '[t]oggle [c]overage signs' })
+vim.keymap.set('n', '<leader>tC', function()
+    require('coverage').clear()
+end, { desc = '[t]est clear [C]overage signs' })
---
+++ lua/plugins/go-tester.lua
@@ -0,0 +1,77 @@
+return {
+    -- 1. Run tests in nvim
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            {
+                "fredrikaverpil/neotest-golang",
+                version = "*",
+                build = function()
+                    vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait()
+                end,
+            },
+        },
+        keys = {
+            { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end,                      desc = "[t]est run [f]ile" },
+            { "<leader>tA", function() require("neotest").run.run(vim.uv.cwd()) end,                            desc = "[t]est [A]ll files" },
+            { "<leader>tS", function() require("neotest").run.run({ suite = true }) end,                        desc = "[t]est [S]uite" },
+            { "<leader>tn", function() require("neotest").run.run() end,                                        desc = "[t]est [n]earest" },
+            { "<leader>ts", function() require("neotest").summary.toggle() end,                                 desc = "[t]est [s]ummary" },
+            { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "[t]est [o]utput" },
+            { "<leader>tO", function() require("neotest").output_panel.toggle() end,                            desc = "[t]est [O]utput panel" },
+            { "<leader>tt", function() require("neotest").run.stop() end,                                       desc = "[t]est [t]erminate" },
+        },
+        config = function()
+            local config = {
+                runner = "gotestsum",
+            }
+            local neotest_golang_opts = {
+                runner = "go",
+                go_test_args = {
+                    "-v",
+                    "-race",
+                    "-count=1",
+                    "-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
+                },
+            }
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-golang")(neotest_golang_opts),
+                },
+            })
+        end,
+    },
+    -- 2. Explore coverage
+    {
+        "andythigpen/nvim-coverage",
+        version = "*",
+        config = function()
+            require("coverage").setup({
+                auto_reload = true,
+                highlights = {
+                    -- customize highlight groups created by the plugin
+                    covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
+                    uncovered = { fg = "#F07178" },
+                },
+                signs = {
+                    -- use your own highlight groups or text markers
+                    covered = { hl = "CoverageCovered", text = "▎" },
+                    uncovered = { hl = "CoverageUncovered", text = "▎" },
+                },
+                summary = {
+                    -- customize the summary pop-up
+                    min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
+                },
+                lang = {
+                    go = {
+                        coverage_file = vim.fn.getcwd() .. '/coverage.out'
+                    }
+                },
+
+            })
+        end,
+    },
+}
