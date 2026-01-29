88d9e09d7022e602bb0e19c29cf57534e9b4fdae
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2026-01-29 15:02:12 +0000

    Feat: Upgraded to nvim-treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,42 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			ensure_installed = {
-				"go",
-				"python",
-				"c",
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    config = function()
+        -- Basic setup (but highlighting won't auto-enable)
+        require("nvim-treesitter").setup({
+            ensure_installed = {
+                "go",
+                "python",
+                "c",
                 "nix",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"elixir",
-				"heex",
-				"javascript",
-				"html",
-			},
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "elixir",
+                "heex",
+                "javascript",
+                "html",
+            },
             auto_install = true,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+        })
+
+        -- -- MANUALLY enable highlighting for Go files
+        -- vim.api.nvim_create_autocmd("FileType", {
+        --     pattern = "go",
+        --     callback = function()
+        --         vim.treesitter.start()
+        --     end,
+        -- })
+
+        -- Auto-enable for ALL file types
+        vim.api.nvim_create_autocmd("BufReadPost", {
+            pattern = "*",
+            callback = function()
+                -- Start treesitter for the current buffer
+                vim.treesitter.start()
+            end,
+        })
+    end,
 }
