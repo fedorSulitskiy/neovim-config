4421bdb0577f1cd59b9e5c778e864315814d6daf
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2026-01-29 09:23:44 +0000

    Feat: Plugin to auto add&remove go struct tags

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -13,3 +13,7 @@ vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
 vim.keymap.set("n", "<leader>mr", function()
 	require('render-markdown').toggle()
 end, { desc = "Toggle Rendered Markdown" })
+
+-- Go Tagger
+vim.keymap.set("v", "<leader>at", ":AddGoTags<CR>", { desc = "Add Go struct tags", silent = true })
+vim.keymap.set("v", "<leader>rt", ":RemoveGoTags<CR>", { desc = "Remove Go struct tags", silent = true })
