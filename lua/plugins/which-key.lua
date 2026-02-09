a936566ba91a1577d04b0e9eeb003a578a52252a
fedorSulitskiy <f.sulitskiy@gmail.com>
2026-02-09 09:14:57 +0000

    Refactor: Change key combination for which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,18 +1,18 @@
 return {
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        opts = {
-            preset = "helix"
-        },
-        keys = {
-            {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
-            },
-        },
-    }
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			preset = "helix",
+		},
+		keys = {
+			{
+				"<leader>wk",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
 }
