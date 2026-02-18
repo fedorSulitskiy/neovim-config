408cfb5022fe42bdea18ae8523bbd0f2b71a1bcd
fedorSulitskiy <f.sulitskiy@gmail.com>
2026-02-18 23:21:31 +0000

    Refactor: Formatted init.lua

--- init.lua
+++ init.lua
@@ -3,20 +3,20 @@
 -- If not create it and git clone lazy there
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.api.nvim_create_autocmd("TermOpen", {
-    pattern = "*",
-    command = "setlocal timeoutlen=0 ttimeoutlen=0",
+	pattern = "*",
+	command = "setlocal timeoutlen=0 ttimeoutlen=0",
 })
 
 require("vim-options")
