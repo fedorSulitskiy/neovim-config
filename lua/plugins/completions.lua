1d14d7b1a299e474bc1249445bd9a52d7f4d008f
fedorSulitskiy <f.sulitskiy@gmail.com>
2026-06-04 22:13:01 +0100

    Feat: Select first completion by Enter without a new line

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -35,8 +35,12 @@ return {
 					-- Enter to confirm completion without newline
 					["<CR>"] = cmp.mapping({
 						i = function(fallback)
-							if cmp.visible() and cmp.get_active_entry() then
-								cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+							if cmp.visible() then
+								if cmp.get_active_entry() then
+									cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+								else
+									cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+								end
 							else
 								fallback()
 							end
