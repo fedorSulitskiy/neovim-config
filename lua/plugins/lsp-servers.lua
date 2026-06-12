4533fcef53998f21c324ca5e2f4e6e5417fac341
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2026-06-12 18:49:10 +0100

    Feat: Improve nix lsp

--- lua/plugins/lsp-servers.lua
+++ lua/plugins/lsp-servers.lua
@@ -1,55 +1,71 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- 1. register the servers
-			vim.lsp.config("lua_ls", {
-				capabilities = capabilities,
-				settings = { Lua = { workspace = { checkThirdParty = false } } },
-			})
+            -- 1. register the servers
+            vim.lsp.config("lua_ls", {
+                capabilities = capabilities,
+                settings = { Lua = { workspace = { checkThirdParty = false } } },
+            })
 
-			vim.lsp.config("gopls", {
-				capabilities = capabilities,
-				settings = { gopls = { analyses = { unusedparams = true }, staticcheck = true } },
-			})
+            vim.lsp.config("gopls", {
+                capabilities = capabilities,
+                settings = { gopls = { analyses = { unusedparams = true }, staticcheck = true } },
+            })
 
-			vim.lsp.config("pyright", {
-				capabilities = capabilities,
-				settings = {
-					pyright = { autoImportCompletion = true },
-					python = {
-						analysis = {
-							autoSearchPaths = true,
-							useLibraryCodeForTypes = true,
-						},
-					},
-				},
-			})
+            vim.lsp.config("pyright", {
+                capabilities = capabilities,
+                settings = {
+                    pyright = { autoImportCompletion = true },
+                    python = {
+                        analysis = {
+                            autoSearchPaths = true,
+                            useLibraryCodeForTypes = true,
+                        },
+                    },
+                },
+            })
 
-			vim.lsp.config("nixd", {
-				capabilities = capabilities,
-				settings = {
-					nixpkgs = {
-						expr = "import <nixpkgs> { }",
-					},
-					formatting = {
-						command = { "nixfmt" },
-					},
-				},
-			})
+            -- nixd: build options config based on hostname
+            local nixd_options = {}
+            local hostname = vim.fn.hostname():gsub("%.local$", "")
 
-			vim.lsp.config("terraformls", {
-				capabilities = capabilities,
-			})
+            -- NixOS machines: point at the system flake for full option completion
+            if hostname == "nixos-btw" or hostname == "nixos-laptop" then
+                nixd_options.nixos = {
+                    expr = '(builtins.getFlake (toString ~/nixos-config)).nixosConfigurations.'
+                        .. hostname
+                        .. ".options",
+                }
+            end
 
-			vim.lsp.config("just-lsp", {
-				capabilities = capabilities,
-			})
+            vim.lsp.config("nixd", {
+                capabilities = capabilities,
+                settings = {
+                    nixd = {
+                        nixpkgs = {
+                            expr = 'import (builtins.getFlake "nixpkgs") { }',
+                        },
+                        formatting = {
+                            command = { "nixfmt" },
+                        },
+                        options = nixd_options,
+                    },
+                },
+            })
 
-			-- 2. enable them
-			vim.lsp.enable({ "lua_ls", "gopls", "pyright", "nixd", "terraformls", "just-lsp" })
-		end,
-	},
+            vim.lsp.config("terraformls", {
+                capabilities = capabilities,
+            })
+
+            vim.lsp.config("just-lsp", {
+                capabilities = capabilities,
+            })
+
+            -- 2. enable them
+            vim.lsp.enable({ "lua_ls", "gopls", "pyright", "nixd", "terraformls", "just-lsp" })
+        end,
+    },
 }
