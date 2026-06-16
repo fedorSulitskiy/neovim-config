e7ab7844419492e635ec357b7c3163fe8392ddca
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2026-06-16 13:18:36 +0100

    Feat: Formatter for JSON

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -1,89 +1,98 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters = {
-				-- Lua
-				stylua = {
-					command = "stylua",
-					args = { "-" },
-					stdin = true,
-				},
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters = {
+                -- Lua
+                stylua = {
+                    command = "stylua",
+                    args = { "-" },
+                    stdin = true,
+                },
 
-				-- Go
-				goimports = {
-					command = "goimports",
-					stdin = true,
-				},
-				gofumpt = {
-					command = "gofumpt",
-					stdin = true,
-				},
-				swag = {
-					command = "swag",
-					args = { "fmt", "-d", "." },
-					stdin = false,
-				},
+                -- Go
+                goimports = {
+                    command = "goimports",
+                    stdin = true,
+                },
+                gofumpt = {
+                    command = "gofumpt",
+                    stdin = true,
+                },
+                swag = {
+                    command = "swag",
+                    args = { "fmt", "-d", "." },
+                    stdin = false,
+                },
 
-				-- Python
-				black = {
-					command = "black",
-					args = { "--quiet", "-" },
-					stdin = true,
-				},
-				isort = {
-					command = "isort",
-					args = { "--quiet", "-" },
-					stdin = true,
-				},
+                -- Python
+                black = {
+                    command = "black",
+                    args = { "--quiet", "-" },
+                    stdin = true,
+                },
+                isort = {
+                    command = "isort",
+                    args = { "--quiet", "-" },
+                    stdin = true,
+                },
 
-				-- Nix
-				alejandra = {
-					command = "alejandra",
-					args = { "--quiet" },
-					stdin = true,
-				},
+                -- Nix
+                alejandra = {
+                    command = "alejandra",
+                    args = { "--quiet" },
+                    stdin = true,
+                },
 
-				-- Terraform
-				terraform = {
-					command = "terraform-ls",
-					args = { "serve" },
-					filetypes = { "terraform", "tf" },
-				},
+                -- Terraform
+                terraform = {
+                    command = "terraform-ls",
+                    args = { "serve" },
+                    filetypes = { "terraform", "tf" },
+                },
 
                 -- Yaml
                 yamlfmt = {
                     command = "yamlfmt",
                     args = { "-formatter", "retain_line_breaks_single=true", "-" },
                     stdin = true,
-					filetypes = { "yaml", "yml" },
+                    filetypes = { "yaml", "yml" },
                 },
-			},
 
-			formatters_by_ft = {
-				lua = { "stylua" },
-				go = { "goimports", "gofumpt", "swag" },
-				python = { "black", "isort" },
-				nix = { "alejandra" },
-				terraform = { "terraform-ls" },
+                -- JSON
+                deno_fmt = {
+                    command = "deno",
+                    args = { "fmt", "--indent-width", "4", "--ext", "json", "-" },
+                    stdin = true,
+                    filetypes = { "json" },
+                },
+            },
+
+            formatters_by_ft = {
+                lua = { "stylua" },
+                go = { "goimports", "gofumpt", "swag" },
+                python = { "black", "isort" },
+                nix = { "alejandra" },
+                terraform = { "terraform-ls" },
                 yaml = { "yamlfmt" },
-			},
+                json = { "deno_fmt" },
+            },
 
-			-- Format on save
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		},
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = { "n", "v" }, -- n = normal mode & v = visual mode
-				desc = "Format buffer",
-			},
-		},
-	},
+            -- Format on save
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+        },
+        keys = {
+            {
+                "<leader>f",
+                function()
+                    require("conform").format({ async = true, lsp_fallback = true })
+                end,
+                mode = { "n", "v" }, -- n = normal mode & v = visual mode
+                desc = "Format buffer",
+            },
+        },
+    },
 }
